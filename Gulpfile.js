d550dbe6f1e0d35fb0c61f99ce15dd3d78b12870
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-19 13:08:05 +0100

    ...

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
-     tsc = require('gulp-typescript')
+    exec = require('child_process').exec;
+     tsc = require('gulp-typescript'),
+  rename = require('gulp-rename'),
   insert = require('gulp-insert');
 
 var header = '/*\r\ndt-init\r\nUtility to generate TypeScript definitions' +
 ' and test stubs.\r\nCopyright 2015 Sam Saint-Pettersen.\r\n\r\n' +
 'Released under the MIT License.\r\n*/\r\n';
 
-gulp.task('default', function() {
-    return gulp.src('*.ts')
+gulp.task('lib', function() {
+    return gulp.src('dt-init-lib.ts')
     .pipe(tsc({
 	module: 'commonjs',
 	removeComments: true
@@ -15,3 +17,20 @@ gulp.task('default', function() {
     .pipe(insert.prepend(header))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('bin', function() { 
+    return gulp.src('dt-init.ts')
+    .pipe(tsc({
+	module: 'commonjs',
+	removeComments: true
+    }))
+    .pipe(insert.prepend(header))
+    .pipe(insert.prepend('#!/usr/bin/env node\r\n'))
+    .pipe(gulp.dest('.'))
+    .pipe(rename('dt-init'))
+    .pipe(gulp.dest('.'));
+    exec('chmod +x dt-init', function(){ console.log('chmod +x dt-init'); });
+});
+
+gulp.task('default', ['lib', 'bin'], function(){});
+
--- dt-init.cmd
+++
@@ -1,3 +0,0 @@
-@node ./dt-init.js %*
-
-
